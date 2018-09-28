126244e63329d7156f2cba2a79b76430f1d542fa
rusong.huan <rusong.huan@mtime.com>
2018-09-28 15:25:02 +0800

    readme.md

---
+++ landmark64/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  landmark64
+//
+//  Created by 宦如松 on 2018/9/27.
+//  Copyright © 2018年 宦如松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
